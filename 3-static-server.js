b5d865e5b8075f1fa71f3cfb3030061228e0900c
sidestepism <fushimi.ryohe.i@gmail.com>
2014-11-04 16:30:15 +0900

    final commit

--- 3-static-server.js
+++ 3-static-server.js
@@ -1,8 +1,13 @@
 // express を使った静的なサーバのサンプル
 
-var express = require("express"),
-    app = express(),
-	port = process.env.PORT || 8000;
+// express ライブラリを読み込み
+var express = require("express");
+
+// アプリケーションオブジェクトを作成
+var app = express();
+
+// ポートを定義 (PORT という環境変数があったらそれを使う)
+var port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/public'));
 
